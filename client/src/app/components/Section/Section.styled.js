048ca2389bdaba4dd7d7f31f16fc41a13765bbaa
Dariusz Syncerek <dsyncerek@outlook.com>
2019-02-21 09:51:54 +0100

    Fix

--- client/src/app/components/Section/Section.styled.js
+++ client/src/app/components/Section/Section.styled.js
@@ -1,11 +1,11 @@
-import { padding, rgba } from 'polished';
+import { padding } from 'polished';
 import styled, { css } from 'styled-components';
 
 export const SectionStyled = styled.div`
   ${props => padding(props.theme.gutter * 10, props.theme.gutter * 3)};
   
   ${props => props.isOdd && css`
-    background: ${rgba(props.theme.colorBgReversed, .95)};
+    background: ${props.theme.colorBgReversed};
     color: ${props.theme.colorTextReversed};
   `}
 `;
