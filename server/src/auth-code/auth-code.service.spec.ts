4a110d3d3742e1a6d6fc079391b58d88c9b999bc
dsyncerek <dsyncerek@outlook.com>
2019-11-04 13:15:35 +0100

    Add test step to workflow

--- server/src/auth-code/auth-code.service.spec.ts
+++ server/src/auth-code/auth-code.service.spec.ts
@@ -15,4 +15,37 @@ describe('AuthCodeService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('generateAuthCode()', () => {
+    it('should return correct auth code', () => {
+      const authCode = service.generateAuthCode('shared_secret');
+
+      expect(authCode).toMatch(/[\w\d]{5}/);
+    });
+  });
+
+  describe('getValidity()', () => {
+    it('should return validity from correct range', () => {
+      const validity = service.getValidity();
+
+      expect(validity).toBeGreaterThan(0);
+      expect(validity).toBeLessThanOrEqual(30000);
+    });
+
+    it('should return correct validity for timestamp less than validity time', () => {
+      jest.spyOn(Date, 'now').mockReturnValue(10000);
+
+      const validity = service.getValidity();
+
+      expect(validity).toBe(20000);
+    });
+
+    it('should return correct validity for timestamp greater that validity time', () => {
+      jest.spyOn(Date, 'now').mockReturnValue(50000);
+
+      const validity = service.getValidity();
+
+      expect(validity).toBe(10000);
+    });
+  });
 });
