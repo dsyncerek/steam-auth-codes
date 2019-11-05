ce4c8aea9219db240e9e28fd54e18fca6fbf5bdd
dsyncerek <dsyncerek@outlook.com>
2019-11-05 12:50:39 +0100

    Sync useDecrease with clock

--- client/src/hooks/useDecrease.js
+++ client/src/hooks/useDecrease.js
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react';
 
-export default (initialValue, decreaseInterval) => {
+export default (initialValue, decreaseInterval = 1000) => {
   const [value, setValue] = React.useState(0);
 
   useEffect(() => {
     setValue(initialValue);
 
-    const handler = setInterval(() => {
-      setValue(currentValue => currentValue - decreaseInterval);
-    }, decreaseInterval);
+    const reminder = initialValue % decreaseInterval;
+
+    let handler = setTimeout(() => {
+      setValue(currentValue => currentValue - reminder);
+
+      handler = setInterval(() => {
+        setValue(currentValue => currentValue - decreaseInterval);
+      }, decreaseInterval);
+    }, reminder);
 
     return () => clearInterval(handler);
   }, [initialValue, decreaseInterval]);
