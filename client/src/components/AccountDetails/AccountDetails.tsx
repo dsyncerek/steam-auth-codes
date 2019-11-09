d6b5d11e0bac53186f39e13d090aac721d8f34e9
Dariusz Syncerek <dsyncerek@outlook.com>
2019-11-09 14:07:39 +0100

    Refactor

--- client/src/components/AccountDetails/AccountDetails.tsx
+++ client/src/components/AccountDetails/AccountDetails.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
-import useDecrease from '../../hooks/useDecrease';
-import useTimeDifference from '../../hooks/useTimeDifference';
+import useAuthCodeValidity from '../../hooks/useAuthCodeValidity';
 import SteamAccount from '../../models/steam-account';
 import { AccountDetailsStyled, BarStyled, CodeStyled, UsernameStyled } from './AccountDetails.styled';
 
@@ -13,8 +12,7 @@ type AccountDetailsProps = {
 };
 
 const AccountDetails: FC<AccountDetailsProps> = ({ account: { username, authCode } }) => {
-  const difference = useTimeDifference(authCode.generatedAt);
-  const currentValidity = useDecrease(authCode.validity - difference, codeDecreaseInterval);
+  const currentValidity = useAuthCodeValidity(authCode, codeDecreaseInterval);
 
   const barWidth = (currentValidity * 100) / codeValidityTime;
   const isEnding = currentValidity <= codeEndingTime;
---
+++ client/src/hooks/useAuthCodeValidity.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import AuthCode from '../models/auth-code';
+
+export default (authCode: AuthCode, decreaseInterval: number = 1000): number => {
+  const [currentValidity, setCurrentValidity] = useState(0);
+
+  useEffect(() => {
+    const timeDiff = Date.now() - authCode.generatedAt;
+    const fixedValidity = authCode.validity - timeDiff;
+
+    setCurrentValidity(fixedValidity);
+
+    const reminder = fixedValidity % decreaseInterval;
+
+    let handler = setTimeout(() => {
+      setCurrentValidity(value => value - reminder);
+
+      handler = setInterval(() => {
+        setCurrentValidity(value => value - decreaseInterval);
+      }, decreaseInterval);
+    }, reminder);
+
+    return () => clearInterval(handler);
+  }, [authCode, decreaseInterval]);
+
+  return currentValidity;
+};
