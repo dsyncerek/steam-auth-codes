c83dc971c005c2d5aa5373f1f8528d91b901d398
Dariusz Syncerek <dsyncerek@outlook.com>
2019-12-02 22:30:40 +0100

    Add Service Worker and manifest.json

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { register } from 'register-service-worker';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+register('/service-worker.js');
