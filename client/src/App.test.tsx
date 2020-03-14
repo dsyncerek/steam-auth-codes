b278661cb4026e12e06a09fd211e13f6503a230f
Dariusz Syncerek <dsyncerek@outlook.com>
2020-03-14 14:30:50 +0100

    fix

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import App from './App';
+import { App } from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
